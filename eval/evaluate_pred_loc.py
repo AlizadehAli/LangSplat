b975d188b4057dcb172b16ac3c90addd999e3a1a
Ali Alizadeh <a.alizade@live.com>
2025-02-28 17:28:54 -0500

    Add prediction-only localization evaluation script.

--- eval/evaluate_pred_loc.py
+++ eval/evaluate_pred_loc.py
@@ -21,7 +21,7 @@ sys.path.append("..")
 import colormaps
 from autoencoder.model import Autoencoder
 from openclip_encoder import OpenCLIPNetwork
-from utils import smooth, colormap_saving, vis_mask_save, polygon_to_mask, stack_mask, show_result
+from utils import smooth, colormap_saving, vis_mask_save, show_result
 
 
 def get_logger(name, log_file=None, log_level=logging.INFO, file_mode='w'):
@@ -41,50 +41,6 @@ def get_logger(name, log_file=None, log_level=logging.INFO, file_mode='w'):
         logger.addHandler(handler)
     logger.setLevel(log_level)
     return logger
-
-
-def eval_gt_lerfdata(json_folder: Union[str, Path] = None, ouput_path: Path = None) -> Dict:
-    """
-    organise lerf's gt annotations
-    gt format:
-        file name: frame_xxxxx.json
-        file content: labelme format
-    return:
-        gt_ann: dict()
-            keys: str(int(idx))
-            values: dict()
-                keys: str(label)
-                values: dict() which contain 'bboxes' and 'mask'
-    """
-    gt_json_paths = sorted(glob.glob(os.path.join(str(json_folder), 'frame_*.json')))
-    img_paths = sorted(glob.glob(os.path.join(str(json_folder), 'frame_*.jpg')))
-    gt_ann = {}
-    for js_path in gt_json_paths:
-        img_ann = defaultdict(dict)
-        with open(js_path, 'r') as f:
-            gt_data = json.load(f)
-        
-        h, w = gt_data['info']['height'], gt_data['info']['width']
-        idx = int(gt_data['info']['name'].split('_')[-1].split('.jpg')[0]) - 1 
-        for prompt_data in gt_data["objects"]:
-            label = prompt_data['category']
-            box = np.asarray(prompt_data['bbox']).reshape(-1)           # x1y1x2y2
-            mask = polygon_to_mask((h, w), prompt_data['segmentation'])
-            if img_ann[label].get('mask', None) is not None:
-                mask = stack_mask(img_ann[label]['mask'], mask)
-                img_ann[label]['bboxes'] = np.concatenate(
-                    [img_ann[label]['bboxes'].reshape(-1, 4), box.reshape(-1, 4)], axis=0)
-            else:
-                img_ann[label]['bboxes'] = box
-            img_ann[label]['mask'] = mask
-            
-            # # save for visulsization
-            save_path = ouput_path / 'gt' / gt_data['info']['name'].split('.jpg')[0] / f'{label}.jpg'
-            save_path.parent.mkdir(exist_ok=True, parents=True)
-            vis_mask_save(mask, save_path)
-        gt_ann[f'{idx}'] = img_ann
-
-    return gt_ann, (h, w), img_paths
 
 
 def activate_stream(sem_map, 
